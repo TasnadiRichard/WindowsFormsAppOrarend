ecfd9361171620a8ba40ff6e40cb66ae9e1a695f
TasnadiRichard <112942145+TasnadiRichard@users.noreply.github.com>
2023-12-10 10:07:02 +0100

    Add files via upload

---
+++ WindowsFormsAppOrarend/FormTanarbelepes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppOrarend
+{
+    public partial class FormTanarbelepes : Form
+    {
+        public FormTanarbelepes()
+        {
+            InitializeComponent();
+        }
+    }
+
+    private void button_regisztracio_Click(object sender, EventArgs e)
+    {
+        FormRegisztracio regisztracio = new FormRegisztracio();
+            if (Program.db.validUser(textBox_felhasznalonev.Text, textBoxjelszo.Text) >= 0)
+            {
+                Program.formRegisztracio.Show();
+            }
+            register.Show();
+    }
+
+    private void button_bejelentkezes_Click(object sender, EventArgs e)
+        {
+            if (Program.db.validUser(textBox_felhasznalonev.Text, textBoxjelszo.Text) >= 0)
+            {
+                Program.formOrak.Show();
+            }
+        }
+}
